b58575f339646f1e35fd4be5f1d7564c21b343c8
deffointern6 <deffointern6@gmail.com>
2026-06-23 17:28:58 +0530

    Redis and Database Configured

--- settings/config.py
+++ settings/config.py
@@ -1,16 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import AnyUrl
 
 class Settings(BaseSettings):
     # Required variable (boot fails instantly if missing from .env or system env)
     hf_token: str
+    database_url: AnyUrl
+    redis_url: str = "redis://localhost:6379/0"
     
-    # defaults
-    ocr_endpoint: str = "https://api-inference.huggingface.co/models/mock-ocr"
-    voice_endpoint_id: str = "mock-voice-id"
-    runpod_key: str = "mock_key"
-
     # Automatically read from a local .env configuration file
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8",
+        case_sensitive=False
+        )
 
 # Create a singleton instance to be used across the app
 settings = Settings()
